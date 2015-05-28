5565253832a03ab0616c95263be77a21f1cba70c
Ignacio Cuevas <nacho_boom_7stars@hotmail.com>
2015-05-28 13:26:14 -0300

    Terminado Patricia Tree, creada clase para los autómatas.

--- src/patriciatree/Edge.java
+++ src/patriciatree/Edge.java
@@ -1,19 +1,35 @@
 package patriciatree;
 
+import java.util.ArrayList;
+
 public class Edge {
-	private String value;
+	private String string;
 	private AbstractNode next;
 	
-	public Edge(String value, AbstractNode next) {
-		this.value = value;
+	public Edge(String string, AbstractNode next) {
+		this.string = string;
 		this.next = next;
 	}
 	
-	public String getValue() {
-		return value;
+	public String getString() {
+		return string;
 	}
 	
 	public AbstractNode getNext() {
 		return next;
+	}
+
+	void split(String match, int index, String difference) {
+		String oldText = string.substring(match.length());
+		String newText = string.substring(0, match.length());
+		Edge oldEdge = new Edge(oldText, this.next);
+		Edge newEdge = new Edge(difference, new LeafNode(index));
+		ArrayList<Edge> newEdges = new ArrayList<Edge>();
+		if (!oldText.equals(""))
+			newEdges.add(oldEdge);
+		newEdges.add(newEdge);
+		this.string = newText;
+		this.next = new InnerNode(newEdges);
+		
 	}
 }
--- src/patriciatree/InnerNode.java
+++ src/patriciatree/InnerNode.java
@@ -8,8 +8,61 @@ public class InnerNode extends AbstractNode {
 	}
 
 	@Override
-	public LeafNode insert(String word) {
-		// TODO Auto-generated method stub
-		return null;
+	public String findLongestMatch(String word) {
+		StringBuilder result = new StringBuilder();
+		for (Edge edge : edges) {
+			String path = edge.getString();
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < Math.min(word.length(), path.length()); i++) {
+				if (word.charAt(i) != path.charAt(i))
+					break;
+				sb.append(word.charAt(i));
+			}
+			String commonString = sb.toString();
+			if (commonString.length() == 0)
+				continue;
+			result.append(path);
+			String nextWord = word.length() > path.length() ? word.substring(path.length()) : "";
+			result.append(edge.getNext().findLongestMatch(nextWord));
+			return result.toString();
+		}
+		return getAnyWord();
 	}
+
+	@Override
+	public String getAnyWord() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(edges.get(0).getString());
+		sb.append(edges.get(0).getNext().getAnyWord());
+		return sb.toString();
+	}
+
+	@Override
+	public void insert(String match, int index, String difference, Edge origin) {
+		if (match.equals("")) {
+			LeafNode newNode = new LeafNode(index);
+			Edge newEdge = new Edge(difference, newNode);
+			edges.add(newEdge);
+			return;
+		}
+		for (Edge edge : edges) {
+			String path = edge.getString();
+			if (match.length() >= path.length() && match.substring(0, path.length()).equals(path)) {
+				edge.getNext().insert(match.substring(path.length()), index, difference, edge);
+				return;
+			}
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < Math.min(match.length(), path.length()); i++) {
+				if (match.charAt(i) != path.charAt(i))
+					break;
+				sb.append(match.charAt(i));
+			}
+			String commonString = sb.toString();
+			if (commonString.length() == 0)
+				continue;
+			edge.split(commonString, index, match.substring(commonString.length()).concat(difference));
+			return;
+		}
+	}
+	
 }
